1c0a0bb53232420f1e719f4f69888f2211ded839
Trang <hoangthiminhtrang304@gmail.com>
2019-06-04 16:18:08 +0700

    set the player info and save the player info to the file

---
+++ lib/player.rb
@@ -0,0 +1,63 @@
+require "yaml"
+
+class Player
+
+    attr_accessor :correct_point, :incorrect_point, :guess_word
+    attr_reader :name
+
+    def initialize(name)
+        @name = name.downcase
+        @correct_point = 0
+        @incorrect_point = 0
+        @secret_word = select_secret_word
+        @guess_word = generate_guess_word(@secret_word)
+    end
+
+    def display_guess_word
+        @guess_word.join(" ")        
+    end
+
+    def save_game
+        player = YAML.dump(self)
+        p self
+        p player
+        Dir.mkdir("saved_game") unless Dir.exist?("saved_game")
+        file_name = "saved_game/#{self.name}.txt"
+        File.open(file_name, "w"){ |file| file.puts player}        
+    end    
+
+    def self.load_game(name)
+        file_name = "saved_game/#{name}.txt"
+        json = File.open(file_name, "r"){ |file| file.read }
+        player = YAML.load(json)
+    end
+    
+    private
+
+    def generate_guess_word(secret_word)
+        word = secret_word.split("")
+        for i in (0..word.length-1) do
+            next if [2,5,8,11].include?(i)
+            word[i] = "_"
+        end
+        word.join(" ")        
+    end
+
+    def select_secret_word
+        dictionary = generate_secret_word
+        index = rand(0..dictionary.length)
+        dictionary[index].downcase
+    end
+
+    def generate_secret_word
+        dictionary = File.open("5text.txt", "r"){ |text| text.read}
+        dictionary.split("\n").select{ |word| (5..12) === word.length}
+        
+    end
+end
+
+
+new_player = Player.new "Trang"
+new_player.save_game
+player = Player.load_game "trang"
+p player
